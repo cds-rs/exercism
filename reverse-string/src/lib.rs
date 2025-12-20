d483a10e7ec4dfdd97e8b8fd148b91dabd0ad7db
cds-amal <cds.sudama@gmail.com>
2025-12-20 18:29:14 -0500

    reverse-string: tickle solution

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,4 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
+    // why is this failing to compile on exercism?
     input.graphemes(true).rev().collect()
 }
