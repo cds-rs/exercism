8b38d80eb76a72770fc6865fe8ea59d3c44bd6d1
cds-amal <cds.sudama@gmail.com>
2025-12-20 14:28:50 -0500

    bob: single pass

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,24 +1,34 @@
-#![allow(dead_code)]
 #[derive(Debug)]
-struct Sentiment<'a> {
-    phrase: &'a str,
+struct Sentiment {
     is_question: bool,
     is_yelling: bool,
     is_silent: bool,
 }
 
-impl<'a> Sentiment<'a> {
-    pub fn new(phrase: &'a str) -> Self {
-        Self {
-            phrase,
-            is_silent: phrase.chars().all(|c| c.is_whitespace()),
-            is_question: phrase.trim().ends_with('?'),
-            is_yelling: {
-                let has_letters = phrase.chars().any(|c| c.is_ascii_alphabetic());
-                let all_uppercase = phrase.chars().filter(|c| c.is_alphabetic())
-                                          .all(|c| c.is_ascii_uppercase());
-                has_letters && all_uppercase
+impl Sentiment {
+
+    pub fn new(phrase: &str) -> Self {
+        let mut has_alpha = false;
+        let mut has_lower = false;
+        let mut has_non_ws = false;
+        let mut last_non_ws: Option<char> = None;
+
+        // Single pass
+        for c in phrase.chars() {
+            if !c.is_whitespace() {
+                has_non_ws = true;
+                last_non_ws = Some(c);
             }
+            if c.is_ascii_alphabetic() {
+                has_alpha = true;
+                has_lower |= c.is_ascii_lowercase();
+            }
+        }
+
+        Self {
+            is_silent: !has_non_ws,
+            is_question: last_non_ws == Some('?'),
+            is_yelling: has_alpha && !has_lower,
         }
     }
 }
@@ -27,12 +37,11 @@ impl<'a> Sentiment<'a> {
 
 pub fn reply(message: &str) -> &str {
     let s = Sentiment::new(message);
-    dbg!(&s);
     match (s.is_silent, s.is_question, s.is_yelling) {
         (false, true, false) => "Sure.",
         (false, false, true) => "Whoa, chill out!",
         (false, true, true) => "Calm down, I know what I'm doing!",
         (false, false, false) => "Whatever.",
-        (_, _, _) => "Fine. Be that way!",
+        _ => "Fine. Be that way!",
     }
 }
