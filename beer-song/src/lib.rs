248f8e58f1209f6be1023194f629347f58114939
cds-amal <cds.sudama@gmail.com>
2025-12-20 21:16:38 -0500

    beer: simplify

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,10 +1,9 @@
 use std::borrow::Cow;
 
-fn bottle_phrase(n: u32, capitalize: bool) -> Cow<'static, str> {
-    match (n, capitalize) {
-        (0, true) => Cow::Borrowed("No more bottles"),
-        (0, false) => Cow::Borrowed("no more bottles"),
-        (1, _) => Cow::Borrowed("1 bottle"),
+fn bottle_phrase(n: u32) -> Cow<'static, str> {
+    match n {
+        0 => Cow::Borrowed("no more bottles"),
+        1 => Cow::Borrowed("1 bottle"),
         _ => Cow::Owned(format!("{n} bottles")),
     }
 }
@@ -16,15 +15,19 @@ pub fn verse(n: u32) -> String {
         _ => ("Take one down and pass it around", n - 1),
     };
 
-    format!(
-        "{} of beer on the wall, {} of beer.\n{}, {} of beer on the wall.\n",
-        bottle_phrase(n, true),
-        bottle_phrase(n, false),
-        action,
-        bottle_phrase(next, false)
-    )
+    let bottles = bottle_phrase(n);
+    let remaining = bottle_phrase(next);
+
+    match n {
+        0 => format!( "No more bottles of beer on the wall, {bottles} of beer.\n{action}, {remaining} of beer on the wall.\n"),
+        _ => format!( "{bottles} of beer on the wall, {bottles} of beer.\n{action}, {remaining} of beer on the wall.\n"),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    (end..=start).rev().map(verse).collect::<Vec<_>>().join("\n")
+    (end..=start)
+        .rev()
+        .map(verse)
+        .collect::<Vec<_>>()
+        .join("\n")
 }
