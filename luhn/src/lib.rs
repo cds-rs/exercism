c760dd8c49a4385fe8d9dfb37acaf1711515372f
cds-amal <cds.sudama@gmail.com>
2025-12-21 11:43:05 -0500

    luhn: second pass

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,29 +1,27 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if code.len() < 2 {
-        return false;
-    }
+    let digits: Vec<u32> = match code
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| c.to_digit(10))
+        .collect::<Option<Vec<_>>>()
+    {
+        Some(d) if d.len() > 1 => d,
+        _ => return false,
+    };
 
-    let mut sum = 0;
-    let mut i = 0;
-    for ch in code.chars().rev() {
-        if ch.is_whitespace() {
-            continue;
-        }
-
-        if !ch.is_ascii_digit() {
-            return false;
-        }
-
-        let mut dig = ch.to_digit(10).unwrap();
-        if i % 2 == 1 {
-            dig *= 2;
-            if dig > 9 {
-                dig -= 9;
+    let sum: u32 = digits
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, &dig)| {
+            if i % 2 == 1 {
+                if dig * 2 > 9 { dig * 2 - 9 } else { dig * 2 }
+            } else {
+                dig
             }
-        }
-        sum += dig;
-        i += 1;
-    }
-    if i > 1 { sum % 10 == 0 } else { false }
+        })
+        .sum();
+
+    sum % 10 == 0
 }
