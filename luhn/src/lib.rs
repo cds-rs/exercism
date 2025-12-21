1e7c3f88a1daa0d4d831411a013f5393f4496a2f
cds-amal <cds.sudama@gmail.com>
2025-12-21 14:21:01 -0500

    luhn: 4 - is_multiple_of

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,25 +1,24 @@
-const DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+static DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
 
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let digits: Vec<u32> = match code
+    let Some(digits) = code
         .chars()
         .filter(|c| !c.is_whitespace())
         .map(|c| c.to_digit(10))
         .collect::<Option<Vec<_>>>()
-    {
-        Some(d) if d.len() > 1 => d,
-        _ => return false,
+    else {
+        return false;
     };
 
-    let sum: u32 = digits
-        .iter()
-        .rev()
-        .enumerate()
-        .map(|(i, &dig)| {
-            if i % 2 == 1 { DOUBLED[dig as usize] } else { dig }
-        })
-        .sum();
-
-    sum % 10 == 0
+    digits.len() > 1
+        && digits
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(i, &dig)| {
+                if i % 2 == 1 { DOUBLED[dig as usize] } else { dig }
+            })
+            .sum::<u32>()
+            .is_multiple_of(10)
 }
