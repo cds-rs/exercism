2b85cee4e3a9f7db97eb9fa7376d51927dcd2146
cds-amal <cds.sudama@gmail.com>
2025-12-21 03:27:31 -0500

    all-your-base: soln

--- all-your-base/src/lib.rs
+++ all-your-base/src/lib.rs
@@ -5,36 +5,41 @@ pub enum Error {
     InvalidDigit(u32),
 }
 
-///
-/// Convert a number between two bases.
-///
-/// A number is any slice of digits.
-/// A digit is any unsigned integer (e.g. u8, u16, u32, u64, or usize).
-/// Bases are specified as unsigned integers.
-///
-/// Return the corresponding Error enum if the conversion is impossible.
-///
-///
-/// You are allowed to change the function signature as long as all test still pass.
-///
-///
-/// Example:
-/// Input
-///   number: &[4, 2]
-///   from_base: 10
-///   to_base: 2
-/// Result
-///   Ok(vec![1, 0, 1, 0, 1, 0])
-///
-/// The example corresponds to converting the number 42 from decimal
-/// which is equivalent to 101010 in binary.
-///
-///
-/// Notes:
-///  * The empty slice ( "[]" ) is equal to the number 0.
-///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
-///    However, your function must be able to process input with leading 0 digits.
-///
+fn to_decimal(digits: &[u32], base: u32) -> Result<u32, Error> {
+    if base < 2 {
+        return Err(Error::InvalidInputBase);
+    }
+
+    let mut value = 0;
+    for digit in digits.iter().skip_while(|&d| *d == 0) {
+        if *digit >= base {
+            return Err(Error::InvalidDigit(*digit));
+        }
+        value = value * base + digit;
+    }
+    Ok(value)
+}
+
+fn from_decimal(mut number: u32, base: u32) -> Vec<u32> {
+    let mut res = Vec::new();
+    loop {
+        let (q, r) = (number / base, number % base);
+        res.push(r);
+        number = q;
+        if number < base {
+            if number > 0 {
+                res.push(number);
+            }
+            break;
+        }
+    }
+    res.into_iter().rev().collect()
+}
+
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    todo!("Convert {number:?} from base {from_base} to base {to_base}")
+    if to_base < 2 {
+        return Err(Error::InvalidOutputBase);
+    }
+    let decimal_value = to_decimal(number, from_base)?;
+    Ok(from_decimal(decimal_value, to_base))
 }
