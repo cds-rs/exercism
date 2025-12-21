4159cd36a3cb4fca44622768d635250d363cf0c2
cds-amal <cds.sudama@gmail.com>
2025-12-21 17:49:21 -0500

    clock: initial soln

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,28 @@
-pub struct Clock;
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Clock {
+    minutes: i32,
+}
+
+const MINUTES_IN_DAY: i32 = 60 * 24;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        let total = hours * 60 + minutes;
+        Self {
+            minutes: total.rem_euclid(MINUTES_IN_DAY),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Self::new(0, self.minutes + minutes)
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let (h, m) = (self.minutes / 60, self.minutes % 60);
+        write!(f, "{h:02}:{m:02}")
     }
 }
