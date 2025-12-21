5e87bb75926810bb2a6f36222622751aab5898ee
cds-amal <cds.sudama@gmail.com>
2025-12-21 12:03:27 -0500

    luhn: 3 - use array as map

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,3 +1,5 @@
+const DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
     let digits: Vec<u32> = match code
@@ -15,11 +17,7 @@ pub fn is_valid(code: &str) -> bool {
         .rev()
         .enumerate()
         .map(|(i, &dig)| {
-            if i % 2 == 1 {
-                if dig * 2 > 9 { dig * 2 - 9 } else { dig * 2 }
-            } else {
-                dig
-            }
+            if i % 2 == 1 { DOUBLED[dig as usize] } else { dig }
         })
         .sum();
 
