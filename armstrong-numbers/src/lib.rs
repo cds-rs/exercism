6d364e9500a3951a0f65a9ce06948b59fe68e1ba
cds-amal <cds.sudama@gmail.com>
2025-12-23 02:50:40 -0500

    armstrong-numbers: 1st iteration

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let mut remaining = num;
+    let mut sum_of_powers = 0;
+    let digit_count = format!("{num}").len() as u32;
+
+    loop {
+        let digit = remaining % 10;
+        sum_of_powers += digit.pow(digit_count);
+        remaining /= 10;
+        if remaining == 0 {
+            break;
+        }
+    }
+    sum_of_powers == num
 }
