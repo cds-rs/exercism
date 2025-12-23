6b169781f4d2b30f5709ba4546dbab13052081f4
cds-amal <cds.sudama@gmail.com>
2025-12-23 02:29:20 -0500

    difference-of-squares: soln

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,11 +1,15 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    // a: sum of first n = n (n-1) / 2
+    // square of sum: a * a
+    let sum_1_to_n = n * (n + 1) / 2;
+    sum_1_to_n * sum_1_to_n
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    // sum of squares: n (n+1) (2n + 1) / 6
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
