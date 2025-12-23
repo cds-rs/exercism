470948f9ddd685af26b619066806bb551cecbad9
cds-amal <cds.sudama@gmail.com>
2025-12-23 14:45:19 -0500

    luhn-from: soln

--- luhn-from/src/lib.rs
+++ luhn-from/src/lib.rs
@@ -1,18 +1,46 @@
-pub struct Luhn;
 
-impl Luhn {
-    pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+#[derive(Debug)]
+pub struct Luhn {
+    is_valid: bool,
+}
+
+/// The numeric types in the tests all implement ToString
+impl<T: ToString> From<T> for Luhn {
+    fn from(input: T) -> Self {
+        Self {
+            is_valid: is_valid_luhn(&input.to_string()),
+        }
     }
 }
 
-/// Here is the example of how the From trait could be implemented
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl From<&str> for Luhn {
-    fn from(input: &str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+impl Luhn {
+    pub fn is_valid(&self) -> bool {
+        self.is_valid
     }
+}
+
+
+/// We're private and we're free
+static DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+
+fn is_valid_luhn(code: &str) -> bool {
+    let Some(digits) = code
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| c.to_digit(10))
+        .collect::<Option<Vec<_>>>()
+    else {
+        return false;
+    };
+
+    digits.len() > 1
+        && digits
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(i, &dig)| {
+                if i % 2 == 1 { DOUBLED[dig as usize] } else { dig }
+            })
+            .sum::<u32>()
+            .is_multiple_of(10)
 }
