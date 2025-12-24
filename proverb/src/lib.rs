1ab621c5606602e9593a168a51453ee576b66832
cds-amal <cds.sudama@gmail.com>
2025-12-24 11:36:05 -0500

    proverb: (1) soln

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
-}
+pub fn build_proverb(list: &[&str]) -> String { // [R] list (borrowed slice)
+    if list.is_empty() {
+        return String::new();
+    }
+
+    let mut proverb = Vec::<String>::new(); // [RWO] proverb (owned)
+    for pair in list.windows(2) {           // [R] pair (borrowed slice)
+        let [want, lost] = pair else { unreachable!() }; // [R] want, lost (borrowed &str)
+        proverb.push(format!("For want of a {want} the {lost} was lost."))
+    } // borrows end: pair, want, lost
+
+    proverb.push(format!("And all for the want of a {}.", list[0]));
+    proverb.join("\n")
+} // borrow ends: list; drop: proverb
