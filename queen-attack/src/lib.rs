f77f5abaad761fa2c3e8bb5c9b9f8719a7b82023
cds-amal <cds.sudama@gmail.com>
2025-12-25 01:37:37 -0500

    queen-attack: (1) soln

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,23 +1,34 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    pos: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        todo!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+        let range = 0..8;
+        if range.contains(&rank) && range.contains(&file) {
+            Some(Self { rank, file })
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        todo!("Given the chess position {position:?}, construct a Queen struct.");
+        Self { pos: position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        todo!("Determine if this Queen can attack the other Queen {other:?}");
+        let delta_x = (self.pos.file - other.pos.file).abs();
+        let delta_y = (self.pos.rank - other.pos.rank).abs();
+        delta_x == 0 || delta_y == 0 || delta_x == delta_y
+
     }
 }
