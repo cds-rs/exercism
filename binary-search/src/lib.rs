b7c27430c17151d5831108c67844130e1b8519a4
cds-amal <cds.sudama@gmail.com>
2025-12-25 00:25:22 -0500

    binary-search: (1) soln

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,5 +1,20 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+pub fn find<T: Ord>(array: impl AsRef<[T]>, key: T) -> Option<usize> {
+    let ary = array.as_ref();
+
+    let mut l = 0;
+    let mut r = ary.len();
+
+    while l < r {
+        let m = (l + r) / 2;
+        if ary[m] == key {
+            return Some(m);
+        }
+
+        if key < ary[m] {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    None
 }
